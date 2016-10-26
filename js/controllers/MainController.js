3320ed5d7b2f7613c52d2432ae70658611fbba67
Caryn Ligon <carynligon@gmail.com>
2016-10-26 11:46:25 -0500

    simple to-do functionality done

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,4 +1,23 @@
 app.controller('MainController', ['$scope', function($scope) {
-  $scope.title = 'Title';
-  $scope.subTitle = 'subtitle';
+  console.log($scope);
+  $scope.title = 'So Many Things To Do Today';
+  $scope.subtitle = 'Let\'s get started!';
+  $scope.tasks = [
+  {
+    task: 'get haircut',
+    timestamp: new Date()
+  }
+];
+  $scope.plusOne =  function(index) {
+    $scope.products[index].likes += 1;
+  };
+  $scope.minusOne = function(index) {
+    $scope.products[index].dislikes += 1;
+  };
+  $scope.addTask = function(index) {
+    $scope.tasks = $scope.tasks.concat({
+      task: document.querySelector('.task-input').value,
+      timestamp: new Date()
+    });
+  }
 }]);
